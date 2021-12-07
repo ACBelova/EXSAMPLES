5b25746fbd462981631918163003f321d43f07d0
Belova <sonia333@yandex.ru>
2021-12-07 14:38:26 +0700

    add massive

---
+++ exsample014_Massive/Program.cs
@@ -0,0 +1,74 @@
+﻿// // работа с массивом (мин, макс)
+
+// int[] arr = { 1, 5, 4, 3, 2, 6, 7, 1, 1 };
+
+// void PrintArray(int[] array)
+// {
+//     int count = array.Length;
+
+//     for (int i = 0; i < count; i++)
+//     {
+//         Console.Write($"{array[i]} ");
+//     }
+// Console.WriteLine();
+// }
+
+// void SelectionSort(int[] array)
+// {
+//     for (int i = 0; i < array.Length-1; i++)
+//     {
+//         int minPosition = i;
+//         for (int j = i + 1; j < array.Length; j++)
+//         {
+//             if (array[j] < array[minPosition]) minPosition = j;
+//         }
+
+
+//         int temporrary = array[i];
+//         array[i] = array[minPosition];
+//         array [minPosition] = temporrary;
+//     }
+// }
+
+
+// PrintArray(arr);
+// SelectionSort(arr);
+
+// PrintArray(arr);
+
+int[] arr = { 1, 5, 4, 3, 2, 6, 7, 1, 1 };
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+Console.WriteLine();
+}
+
+void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length-1; i++)
+    {
+        int maxPosition = i;
+        for (int j = i+1; j < array.Length-1; j++)
+        {
+            if (array[j] > array[maxPosition]) maxPosition = j;
+        }
+
+
+        int temporrary = array[i];
+        
+        array[i] = array[maxPosition];
+        array [maxPosition] = temporrary;
+    }
+}
+
+
+PrintArray(arr);
+SelectionSort(arr);
+
+PrintArray(arr);
